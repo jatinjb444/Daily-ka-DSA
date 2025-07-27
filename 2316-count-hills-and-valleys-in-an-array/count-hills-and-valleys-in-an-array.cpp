49054bddecf41fbc21c19880e3ea62405d934755
jatinjb444 <jatinjb05@gmail.com>
2025-07-27 13:59:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (50.25%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int countHillValley(vector<int>& nums) {
-        int cnt = 0;
-        vector<int> numsCopy;
-        for(int i = 0 ; i < nums.size(); i++){
-           if(numsCopy.empty() || nums[i] != numsCopy.back())
-              numsCopy.push_back(nums[i]);
-        }
-        for(int i = 1 ; i < numsCopy.size()-1; i++){
-            if(numsCopy[i] < numsCopy[i-1] && numsCopy[i] < numsCopy[i+1]){
-                cnt++;
-            }else  if(numsCopy[i] > numsCopy[i-1] && numsCopy[i] > numsCopy[i+1]){
-                cnt++;
+    int countHillValley(std::vector<int>& nums) {
+        int count = 0;
+        int left = 0;
+
+        for (int i = 1; i < nums.size() - 1; ++i) {
+            if (nums[i] != nums[i + 1]) {
+                if ((nums[i] > nums[left] && nums[i] > nums[i + 1]) ||
+                    (nums[i] < nums[left] && nums[i] < nums[i + 1])) {
+                    ++count;
+                }
+                left = i;
             }
         }
-        return cnt;
+
+        return count;
     }
 };
